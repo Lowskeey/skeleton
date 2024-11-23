b6fc3ffbc28d877d951d101394cd5d5c3d92f642
IGOR <mralabamamason@gmail.com>
2024-11-23 23:12:18 +0300

    headers was updated

--- skeleton.h
+++ skeleton.h
@@ -8,5 +8,8 @@ enum PROGRAM_CODES {
 	ABORTED,
 	ERROR
 }; /* RETURN THESE CODES! */
+void toggle_quotes (bool *quotesEnabled);
+void set_quote(char **quote, char *newText);
+void do_cls();
 void print_skeleton(int whichFrame);
 bool is_playing(char *buff);
